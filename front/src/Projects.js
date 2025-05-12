086cd23c88bd359a3af701660ddcda56068c5b0f
jotabe <danurabe@gmail.com>
2025-05-12 15:43:26 -0300

    project

--- front/src/Projects.js
+++ front/src/Projects.js
@@ -4,14 +4,14 @@ import "./Projects.css"
 import img1 from "./images/1.png"
 import img2 from "./images/2.png"
 import img3 from "./images/3.png"
-import img4 from "./images/4.png"
 
 const Projects = ({translations}) => {
-  const imgs = [img1,img2,img3,img4]
+  const imgs = [img1,img2,img3]
   
   return (
     <div className="projects-container">
       {projects && projects.map((project,index)=>{
+        if(index != 0){
         return <div className="project">
           <div className="left"></div>
           <h2>{project.title}</h2>
@@ -19,6 +19,16 @@ const Projects = ({translations}) => {
           <img className="project-img" src={imgs[index]}/>
           <a className="visit-site" href={project.link} target='_blank'>&gt; {translations.visitSite}</a>
         </div>
+        } else {
+          return <div className="project project-highlight">
+          <div className="left"></div>
+          <h2>{project.title}</h2>
+          <p>{translations.projects[index]}</p>
+          <img className="project-img" src={imgs[index]}/>
+          <a className="visit-site" href={project.link} target='_blank'>&gt; {translations.visitSite}</a>
+        </div>
+        }
+
       })}
     </div>
   )
--- front/src/ProjectsInfo.js
+++ front/src/ProjectsInfo.js
@@ -1,8 +1,7 @@
 const projects = [
     {title:"Fakeverse", content:"Social media app made with Django with AI functionality",link:"https://fakeverse.vercel.app/"},
     {title:"Chadware",content:"Fake e-commerce made with React + Node.js + Express",link:"https://chadware.netlify.app"},
-    {title:"Movision",content:"React movie app that shows information about movies using the TMDB API.",link:"https://movision.netlify.app/"},
-    {title:"Paqui Patelo",content:"Sports e-commerce made with Wordpress using WooCommerce",link:"https://paquipatelo.wuaze.com"},
+    {title:"Movision",content:"React movie app that shows information about movies using the TMDB API.",link:"https://movision.netlify.app/"}
 ]
 
 export default projects
